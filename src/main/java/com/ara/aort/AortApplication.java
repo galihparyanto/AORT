66b19fc57aa630791da1cab972595c830fbf2d3c
juniar_rakhman <juniar.rahman@gmail.com>
2016-08-11 22:41:25 +0700

    Initial commit

---
+++ src/main/java/com/ara/aort/AortApplication.java
@@ -0,0 +1,12 @@
+package com.ara.aort;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AortApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AortApplication.class, args);
+	}
+}
---
+++ src/test/java/com/ara/aort/AortApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ara.aort;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AortApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
